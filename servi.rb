ed9ec2c21e4e686affee1905a21738b766bef538
Kevin Cooper <k.coopr@gmail.com>
2015-07-26 19:15:22 -0400

    updated servi to 1.1

--- servi.rb
+++ servi.rb
@@ -1,9 +1,9 @@
 class Servi < Formula
   desc "Super awesome server initialization script!"
   homepage "https://github.com/kevcooper/servi"
-  url "https://github.com/kevcooper/servi/archive/v1.0.tar.gz"
+  url "https://github.com/kevcooper/servi/archive/v1.1.tar.gz"
   head "https://github.com/kevcooper/servi.git"
-  sha256 "16ca0775b3d36c9ad2619f7653c377ad0c49216402d63199fab8acc8fc464ccf"
+  sha256 "800c842fe8a9fb927f047059308c01a20ae5ae73bafeb7402ecab46998b27458"
 
   def install
     bin.install "servi"
