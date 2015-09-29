f1ec46f31966b577473ed60510c480a80e648689
Kevin Cooper <k.coopr@gmail.com>
2015-09-28 19:03:20 -0700

    updated giti

--- giti.rb
+++ giti.rb
@@ -1,9 +1,9 @@
 class Giti < Formula
   desc "Super awesome git initialization script!"
   homepage "https://github.com/kevcooper/giti"
-  url "https://github.com/kevcooper/giti/archive/v1.1.tar.gz"
+  url "https://github.com/kevcooper/giti/archive/v1.2.tar.gz"
   head "https://github.com/kevcooper/giti.git"
-  sha256 "c76c04bd057c622e9dcd369f24f07ee40653992326847c1eacaba478810ab9b8"
+  sha256 "63b0ec604a94cbceda6f9a142f981a2c72cf6c6c7691554a292e49b777cccaf2"
 
   def install
     bin.install "giti"
